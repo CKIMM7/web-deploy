f28ed5c42455933e1ea58e91108a7c935d379aa8
YKIM <dyounggkim@gmail.com>
2023-04-20 16:44:21 +0100

    fix for env

--- db.py
+++ db.py
@@ -1,17 +1,30 @@
 import psycopg2
+import os
+
+aws_db_database = os.getenv('aws_db_database')
+aws_db_user = os.getenv('aws_db_user')
+aws_db_password = os.getenv('aws_db_password')
+aws_db_host = os.getenv('aws_db_host')
+
 
 conn = psycopg2.connect(
-    database="testdb",
-    user="postgres",
-    password="postgres",
-    host="database-1.c4rcnm5z6bgf.eu-west-2.rds.amazonaws.com",
+    database=aws_db_database,
+    user=aws_db_user,
+    password=aws_db_password,
+    host=aws_db_host,
     port='5432'
 )
 
 cur = conn.cursor()
-cur.execute("DROP TABLE users;")
+try:
+    cur.execute("DROP TABLE users;")
+except psycopg2.Error as e:
+    print(e.pgcode)
 
-cur.execute("CREATE TABLE users (id serial PRIMARY KEY, name VARCHAR ( 50 ) UNIQUE NOT NULL, email VARCHAR ( 50 ) UNIQUE NOT NULL, guid VARCHAR ( 50 ) UNIQUE NOT NULL, photo VARCHAR ( 100 ) UNIQUE NOT NULL, access_id VARCHAR ( 50 ), secret_id VARCHAR ( 50 ));")
+try:
+    cur.execute("CREATE TABLE users (id serial PRIMARY KEY, name VARCHAR ( 50 ) UNIQUE NOT NULL, email VARCHAR ( 50 ) UNIQUE NOT NULL, guid VARCHAR ( 50 ) UNIQUE NOT NULL, photo VARCHAR ( 100 ) UNIQUE NOT NULL, access_id VARCHAR ( 50 ), secret_id VARCHAR ( 50 ));")
+except psycopg2.Error as e:
+    print(e.pgcode)
 
 cur.execute("INSERT INTO users (name, email, guid, photo, access_id, secret_id) VALUES (%s, %s, %s, %s, %s, %s)",
             ('kim1', 'kim@gmail.com', 'guid', 'photo', '', ''))
